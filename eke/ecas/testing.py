343a40b4200ef225378b893d886583f269d318c4
Sean Kelly <sean.c.kelly@me.com>
2012-02-20 03:45:55 +0000

    work around bizareness

--- eke/ecas/testing.py
+++ eke/ecas/testing.py
@@ -3,18 +3,28 @@
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
 from eke.knowledge.testing import EKE_KNOWLEDGE_FIXTURE
-from eke.site.testing import EKE_SITE_FIXTURE
-from eke.study.testing import EKE_STUDY_FIXTURE
 from plone.app.testing import PloneSandboxLayer, IntegrationTesting, FunctionalTesting
 from plone.testing import z2
 from Products.CMFCore.utils import getToolByName
 
 class EKEECAS(PloneSandboxLayer):
-    defaultBases = (EKE_STUDY_FIXTURE, EKE_SITE_FIXTURE, EKE_KNOWLEDGE_FIXTURE)
+    defaultBases = (EKE_KNOWLEDGE_FIXTURE,)
     def setUpZope(self, app, configurationContext):
         import eke.ecas
         self.loadZCML(package=eke.ecas)
         z2.installProduct(app, 'eke.ecas')
+        
+        # More mystery
+        import eke.study
+        self.loadZCML(package=eke.study)
+        z2.installProduct(app, 'eke.study')
+        import eke.site
+        self.loadZCML(package=eke.site)
+        z2.installProduct(app, 'eke.site')
+        import eke.publications
+        self.loadZCML(package=eke.publications)
+        z2.installProduct(app, 'eke.publications')
+
         import eke.ecas.tests.base
         eke.ecas.tests.base.registerLocalTestData()
     def setUpPloneSite(self, portal):
