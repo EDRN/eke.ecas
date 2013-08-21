b6234549a3fef4a3847d523f9a0f057e1b33c9ce
Sean Kelly <sean.c.kelly@me.com>
2013-08-21 20:57:48 +0000

    Use content-core, add z3cautoinclude; use re-factored base.cfg

--- setup.py
+++ setup.py
@@ -18,9 +18,11 @@ _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
 _license     = 'Proprietary'
 _namespaces  = ['eke']
-_entryPoints = {}
 _zipSafe     = False
 _keywords    = 'web zope plone edrn cancer biomarkers eke ecas datasets data management'
+_entryPoints     = {
+    'z3c.autoinclude.plugin': ['target=plone'],
+}
 _extras = {
     'test': ['plone.app.testing'],
 }
@@ -28,6 +30,7 @@ _externalRequirements = [
     'setuptools',
     'Products.CMFPlone',
     'zope.globalrequest',
+    'z3c.autoinclude',
 ]
 _classifiers = [
     'Development Status :: 4 - Beta',
